adce0fa5265e928cb3748d24be453a42a639d997
René Mihula <mihula@provys.com>
2022-07-27 15:27:05 +0200

    Whisper a Translate

---
+++ Program.cs
@@ -0,0 +1,56 @@
+﻿using Provys.Custom.OTE;
+
+namespace SportInfo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var dict = new SportDict(CreateSampleData);
+
+            Console.WriteLine("Whisper");
+            Debug("Team: ABC", dict.Whisper("ABC", SportDictType.Team, true));
+            Debug("Team: BAN", dict.Whisper("BAN", SportDictType.Team, true));
+            Debug("Team: SP", dict.Whisper("SP", SportDictType.Team, true));
+            Debug("Team: A", dict.Whisper("A", SportDictType.Team, true));
+            Debug("Venue: A", dict.Whisper("A", SportDictType.Venue, true));
+            Debug("Venue: O", dict.Whisper("O", SportDictType.Venue, true));
+            Debug("Team: Σ", dict.Whisper("Σ", SportDictType.Team, false));
+
+            Console.WriteLine();
+            Console.WriteLine("Translate");
+            Debug("Team: ABC (to GR)", dict.Translate("ABC", SportDictType.Team, true));
+            Debug("Team: BANIK (to GR)", dict.Translate("BANIK", SportDictType.Team, true));
+            Debug("Team: BANICEK (to GR)", dict.Translate("BANICEK", SportDictType.Team, true));
+            Debug("Team: BAN (to GR)", dict.Translate("BAN", SportDictType.Team, true));
+            Debug("Team: SPARTA (to GR)", dict.Translate("SPARTA", SportDictType.Team, true));
+            Debug("Team: MOSKVA (to GR)", dict.Translate("MOSKVA", SportDictType.Team, true));
+            Debug("Team: Σ (to EN)", dict.Translate("Σ", SportDictType.Team, false));
+            Debug("Team: Σπάρτη (to EN)", dict.Translate("Σπάρτη", SportDictType.Team, false));
+
+            Console.WriteLine("Bye");
+        }
+
+        private static void Debug(string message, IEnumerable<string> result)
+        {
+            Console.WriteLine(message);
+            Console.WriteLine($"Result ({result.Count()}): {string.Join(", ", result.ToArray())}");
+        }
+
+        private static IEnumerable<SportDictData> CreateSampleData()
+        {
+            var result = new List<SportDictData>();
+
+            result.Add(new SportDictData() { ID = 0, Source = SportDictSource.Import, Type = SportDictType.Team, TitleEN = "BANIK", TitleGR = "Ανθρακωρύχος" });
+            result.Add(new SportDictData() { ID = 1, Source = SportDictSource.Import, Type = SportDictType.Team, TitleEN = "SPARTA PRAHA", TitleGR = "Σπάρτη Πράγα" });
+            result.Add(new SportDictData() { ID = 2, Source = SportDictSource.Import, Type = SportDictType.Team, TitleEN = "SPARTAK MOSKVA", TitleGR = "Σπάρτακος Μόσχα" });
+            
+            result.Add(new SportDictData() { ID = 100, Source = SportDictSource.Import, Type = SportDictType.Venue, TitleEN = "Athenes", TitleGR = "Αθήνα" });
+            result.Add(new SportDictData() { ID = 101, Source = SportDictSource.Import, Type = SportDictType.Venue, TitleEN = "London", TitleGR = "Λονδίνο" });
+            result.Add(new SportDictData() { ID = 102, Source = SportDictSource.Import, Type = SportDictType.Venue, TitleEN = "Ostrava", TitleGR = "Οστράβα" });
+           
+
+            return result;
+        }
+    }
+}
---
+++ SportDict.cs
@@ -0,0 +1,104 @@
+﻿// Modification History
+// ---------------------------------------------------------------------------------
+// 27.07.2022  mihula  [JM220711-06E8226] CR42 - Sports EPG
+
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Provys.Custom.OTE
+{
+    public class SportDict {
+
+        private Func<IEnumerable<SportDictData>> populate;
+        private List<SportDictData>? data = null;
+
+        public SportDict(Func<IEnumerable<SportDictData>> populateFunc)
+        {
+            populate = populateFunc ?? throw new System.ArgumentNullException(nameof(populateFunc));            
+        }
+
+        public void Clear()
+        {
+            if (this.data != null) this.data.Clear();
+            this.data = null;
+        }
+
+        public void PopulateIfEmpty()
+        {
+            if ((this.data?.Count ?? 0) > 0) return;
+
+            this.data = new List<SportDictData>();
+            this.data.AddRange(populate());
+        }
+
+        public IEnumerable<string> Translate(string input, SportDictType type, bool fromEN)
+        {
+            if (string.IsNullOrEmpty(input)) throw new ArgumentNullException(nameof(input));
+
+            PopulateIfEmpty();
+
+            var typedData = from d in this.data where d.Type == type select new TupleFromTo(fromEN ? d.TitleEN : d.TitleGR, fromEN ? d.TitleGR : d.TitleEN); // Teams / Venues...
+
+            // exactmatch returns translation
+            var exactMatch = from d in typedData where string.Compare(input, d.From, StringComparison.InvariantCultureIgnoreCase) == 0 select d.To;
+            if (exactMatch.Any())
+            {
+                return exactMatch;
+            }
+
+            // lets lower the bar (startwith)
+            var startWithMatch = from d in typedData where d.From.StartsWith(input, StringComparison.InvariantCultureIgnoreCase) select d.To;
+            if (startWithMatch.Any())
+            {
+                return startWithMatch;
+            }
+
+            // lets lower the bar even more (substrings)
+            var substringMatch = from d in typedData where d.From.Contains(input, StringComparison.InvariantCultureIgnoreCase) select d.To;
+            if (substringMatch.Any())
+            {
+                return substringMatch;
+            }
+
+            // nothing
+            return new List<string>();
+        }
+
+        public IEnumerable<string> Whisper(string input, SportDictType type, bool fromEN)
+        {
+            PopulateIfEmpty();
+
+            var typedData = from d in this.data where d.Type == type select fromEN ? d.TitleEN : d.TitleGR;
+            var result = from d in typedData where d.Contains(input, StringComparison.InvariantCultureIgnoreCase) select d;
+            return result.ToList();
+        }
+    }
+
+    public enum SportDictType { Unknown, Team, Venue, Event, Group, Match }
+
+    public enum SportDictSource { Unknown, Manual, Import }
+
+    [DebuggerDisplay("{Type}: {TitleEN} <=> {TitleGR} | {Source} | {ID}")]
+    public class SportDictData
+    {
+        public object ID { get; set; }
+        public SportDictType Type { get; set; }
+        public SportDictSource Source { get; set; }
+        public string TitleEN { get; set; }
+        public string TitleGR { get; set; }
+    }
+
+    public class TupleFromTo
+    {
+        public TupleFromTo(string from, string to)
+        {
+            From = from;
+            To = to;
+        }
+
+        public string From { get; }
+        public string To { get; }
+    }
+}
